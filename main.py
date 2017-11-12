3ce4d8f6f9b9b6b3586b0492ca766e052f068c3d
daichi <daichi@DPC2>
2017-11-13 00:57:08 +0900

    ファイル検索システム終了

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import numpy as np
 import cupy as cp
 from PIL import Image
 import math
-
+import re
+import fnmatch
+import os
+from os.path import join, relpath
+from glob import glob
 #xp = np
 xp = cp
 
@@ -12,3 +16,14 @@ from chainer import optimizers
 import chainer.functions as F
 from chainer.links import caffe
 from sympy.printing.pretty.pretty_symbology import xstr
+"""
+for inList in glob.iglob('traindata/*_i.bmp', recursive=True):
+    print(inList)
+"""
+
+tdPath = 'traindata'
+allList = [relpath(x, tdPath) for x in glob(join(tdPath, '*'))]
+inList = fnmatch.filter(allList,"*_i.bmp")
+for i in range(0, len(inList)):
+    t2 = tdPath+'/'+inList[i][:-6]+'_o.bmp'
+    if(os.path.exists(t2) == True):
